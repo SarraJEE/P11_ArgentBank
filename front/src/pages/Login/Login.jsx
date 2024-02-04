04449977240c9d2ec7fbc362ff0897e7fb61b02e
SarraJEE <bgrsarra@gmail.com>
2024-02-04 13:57:10 +0100

    Utilisation du SASS

--- front/src/pages/Login/Login.jsx
+++ front/src/pages/Login/Login.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 function Login() {
     return (
-        <main class="main bg-dark">
+        <main className="main bg-dark">
             <section className="sign-in-content">
                 <i className="fa fa-user-circle sign-in-icon"></i>
                 <h1>Sign In</h1>
